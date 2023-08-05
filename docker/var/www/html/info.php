150e5a993b9f2f12ce152c8030e55568b83df59f
Shota Ito <shota.ito.jp@gmail.com>
2023-08-05 20:01:37 +0900

    必要なファイル一式を追加

---
+++ docker/var/www/html/info.php
@@ -0,0 +1,11 @@
+<!--https://www.php.net/manual/ja/function.phpinfo.php-->
+<?php
+
+// すべての情報を表示します。デフォルトは INFO_ALL です。
+phpinfo();
+
+// モジュール情報だけを表示します。
+// phpinfo(8) としても同じです。
+phpinfo(INFO_MODULES);
+
+?>
